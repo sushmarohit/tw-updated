ec0b0ada89e50dbd5c56dc138a5e1d0afa4926c4
rohitrampla <thakurrohit210302@gmail.com>
2026-03-03 00:56:27 +0530

    chore: update call-to-action text in home JSON and ensure consistency in bundled JSON structure

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -38,7 +38,7 @@ const socialLinks = [
   { name: 'LinkedIn', icon: Linkedin, href: 'https://www.linkedin.com/company/112149124/admin/dashboard/' },
   { name: 'Instagram', icon: Instagram, href: 'https://www.instagram.com/twelfthkey/' },
   { name: 'Facebook', icon: Facebook, href: 'https://www.facebook.com/twelfthkey' },
-  { name: 'WhatsApp', icon: MessageCircle, href: 'https://wa.me/8424885575' },
+  { name: 'WhatsApp', icon: MessageCircle, href: 'https://wa.me/9967193526' },
 ];
 
 const trustBadgeKeys = [
